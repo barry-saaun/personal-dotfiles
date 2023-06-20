cf41b52ecb382fa24e9e1dd703452d31cc89925b
Saaun Sovannpanha <panhaocr@gmail.com>
2023-06-20 13:15:37 +0700

    fix(nvim): removing some plugins, including nvimtree since I have had a better experience using telescope file browsing extension

--- nvim/lua/barry/core/maps.lua
+++ nvim/lua/barry/core/maps.lua
@@ -53,9 +53,6 @@ keymap.set("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>")
 -- vim-maximizer
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window maximization
 
--- nvim-tree
-keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
-
 -- telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")  -- find files within current working directory, respects .gitignore
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")   -- find string in current working directory as you type
